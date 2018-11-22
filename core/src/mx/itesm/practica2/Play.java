e152ac6bd474712d2ffadc73e5b63d981edc061b
HumbertoPerez <32501249+HumbertoPerez0018@users.noreply.github.com>
2018-11-22 08:23:27 -0600

    Add files via upload

--- core/src/mx/itesm/practica2/Play.java
+++ core/src/mx/itesm/practica2/Play.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
@@ -19,6 +21,7 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Array;
@@ -440,6 +443,14 @@ public class Play extends Pantalla {
                 }
             });
             this.addActor(btnContinuar);
+
+            TextButton.TextButtonStyle estilo = new TextButton.TextButtonStyle();
+            estilo.fontColor = Color.BLACK;
+            estilo.font= new BitmapFont(Gdx.files.internal("fonts/Nevermore.fnt"));
+            TextButton btn = new TextButton(String.valueOf(puntos), estilo);
+            btn.setPosition((ANCHO/2) -30, (ALTO/2)-10);
+            this.addActor(btn);
+
         }
 
     }
